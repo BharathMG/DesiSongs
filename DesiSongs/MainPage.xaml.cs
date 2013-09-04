62f7daa3029abbc53942edd938ca6a0498ff82a3
Bharath MG <mgbharath@ymail.com>
2013-09-05 00:46:05 +0530

    Nokia Music client added

--- DesiSongs/MainPage.xaml.cs
+++ DesiSongs/MainPage.xaml.cs
@@ -8,18 +8,72 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using DesiSongs.Resources;
+using Nokia.Music;
+using Windows.Phone.Speech.Synthesis;
+using Windows.Phone.Speech.Recognition;
+using Windows.Phone.Speech.VoiceCommands;
+using Nokia.Music.Types;
 
 namespace DesiSongs
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private MusicClient ms;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
+            ms = new MusicClient("d5117c1efbda09d54b21504d6cf2aa81", "3KeP3DIZIx58fmkutvLJ/AEloJFTu3q7Q9zlqfBJ5U4CsfrLsTGRTrUceacDgEMM");
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        private void searchArtist(object sender, RoutedEventArgs e)
+        {
+            Artist artist=new Artist();
+            ms.SearchArtists(
+  (ListResponse<Artist> artistResponse) =>
+  {
+      if (artistResponse.Count() > 0)
+      {
+           artist = artistResponse.First<Artist>(); //let's assume that the first returned artist is the one we're looking for
+      }
+      else
+      {
+          MessageBox.Show("Sorry, we couldn't find any artist named ");
+      }
+  }, "Akon");
+
+            ms.GetArtistProducts(
+  (ListResponse<Product> productResponse) =>
+  {
+      var songs = productResponse.Where((x) => //productResponse will include all products, not just songs. this will only return tracks and singles
+      {
+          if (x.Category == Category.Track || x.Category == Category.Single)
+          {
+              return true;
+          }
+          else
+          {
+              return false;
+          }
+      }
+      );
+      String ArtistName = "Akon";
+      List<Product> productList = songs.ToList<Product>();
+      if (productList.Count >= 3)
+      {
+          MessageBox.Show("The top songs by " +  ArtistName + productList[0].Name + ", " + productList[1].Name + ", and " + productList[2].Name);
+      }
+      else if (productList.Count >= 1)
+      {
+          MessageBox.Show("The top song by " + ArtistName + " is " + productList[0].Name);
+      }
+      else
+      {
+          MessageBox.Show("Sorry, we don't have song info for " + ArtistName);
+      }
+  }, artist);
         }
 
         // Sample code for building a localized ApplicationBar
---
+++ packages/NokiaMusic.2.4.0/readme.txt
@@ -0,0 +1,13 @@
+The Nokia Music API lets you easily integrate your Windows Phone application with Nokia Music on Nokia Lumia phones or your Windows Store app with Nokia Music for Windows 8.
+
+The API offers two levels of integration; the simplest and quickest to get going is to use the high-level Launcher Tasks,
+with more advanced integration available to perform searches and get recommendations within your app.
+
+Please link back to Nokia Music from your app!
+
+For more information on the API, go to http://nokia.ly/wpmusicapi
+
+Copyright (c) 2012-2013, Nokia
+All rights reserved.
+
+V2.x includes support for Windows 8 and has therefore renamed the Nokia.Music.Phone namespace to Nokia.Music
